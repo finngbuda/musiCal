13c5afa29c9fbeaf4b65fc40edf487ab2584fbf1
Finn Buda <finbud117@gmail.com>
2026-03-27 11:44:10 -0500

    instrument struct fix

--- src/instrument.c
+++ src/instrument.c
@@ -1,5 +1,14 @@
 #include "instrument.h"
+#include <stdlib.h>
 
 // declaring instrument
 // returns an instrument struct which the user can add to the track at their choosing
+
+struct transient_sample * init_transient_sample(unsigned int base_freq, unsigned int amp, int amp_decay_func,int amp_decay_coef, int freq_decay_func, int freq_decay_coef){
+	
+	struct transient_sample * ts = malloc(sizeof(struct transient_sample));
+
+	return ts;
+
+}
 
--- src/instrument.h
+++ src/instrument.h
@@ -23,14 +23,12 @@ enum decay_functions{
 // for now it will all be explicitly defined on the user side4
 // but common effects as well and presets can be used in the future.
 struct transient_sample{
-
 	unsigned int base_freq;
 	unsigned int amp;
-	int amp_decay_func = NOFUNC;
-	int amp_decay_coef = 0;
-	int freq_decay_func = NOFUNC;
-	int freq_decay_coef = 0;
-
+	int amp_decay_func;
+	int amp_decay_coef;
+	int freq_decay_func;
+	int freq_decay_coef;
 };
 
 
@@ -38,3 +36,4 @@ struct transient_sample{
 // USER CALLED FUNCTIONS
 
 struct transient_sample * init_transient_sample(unsigned int base_freq, unsigned int amp, int amp_decay_func,int amp_decay_coef, int freq_decay_func, int freq_decay_coef);
+
