121f0c8820b191c40ef193ba042077fb9a27f403
Finn Buda <finbud117@gmail.com>
2026-03-30 11:42:11 -0500

    finished transient rendering, just need basic song structure to fully test

--- src/instrument.c
+++ src/instrument.c
@@ -4,54 +4,122 @@
 
 
 // internal func declarations
-unsigned int calculate_len(unsigned int amp, int amp_func, int amp_coef){
+float calculate_len(unsigned int amp, int amp_func, float amp_coef){
 
 	switch(amp_func){
 	
 		case LINEAR:
-			return (amp / amp_coef);
+			return (1 / amp_coef);
 
 		case QUADRATIC:
-			return sqrt(amp / amp_coef);
+			return sqrt(1 / amp_coef);
 
 		case EXPONENTIAL:
-			return (5 * amp / amp_coef); // exp decay is asymptotic, so just use e to the -5
+			return (5 / amp_coef); // exp decay is asymptotic, so just use e to the -5
 
 		default:
 			return 0;
-	}
+		}
 
 }
+
+// does actual loop to calculate both frequencies for each sample
+uint16_t * calculate_freqs(unsigned int base, int func, float coef, float len){
+
+	uint16_t * frequencies = malloc((int)(len * 44100) * sizeof(uint16_t)); //allocate space for each sample
+
+	switch(func){
+	
+		case NOFUNC:
+			for(int i = 0; i < (int)(len); i++){
+				frequencies[i] = (uint16_t)base; // set all to base if no freq decay
+			}
+
+		case LINEAR:
+			for(int i = 0; i < (int)(len); i++){
+				frequencies[i] = (uint16_t)LINEAR_DECAY(i, coef, base); // set all to base if no freq decay
+			}
+
+		case QUADRATIC:
+			for(int i = 0; i < (int)(len); i++){
+				frequencies[i] = (uint16_t)QUADRATIC_DECAY(i, coef, base);  // set all to base if no freq decay
+			}
+
+		case EXPONENTIAL:
+			for(int i = 0; i < (int)(len); i++){
+				frequencies[i] =(uint16_t)EXP_DECAY(i, coef, base);  // set all to base if no freq decay
+			}
+
+	}
+
+	return frequencies;
+
+}
+
+
+// does actual loop to calculate both frequencies for each sample
+uint16_t * calculate_vals(unsigned int base, int func, float coef, float len, uint16_t * freqs){
+
+	uint16_t * vals = malloc((int)(len * 44100) * sizeof(uint16_t)); //allocate space for each sample
+
+	switch(func){
+	
+		case NOFUNC:
+			for(int i = 0; i < (int)(len); i++){
+				vals[i] = (uint16_t)base * sin(freqs[i] * i); // set all to base if no freq decay
+			}
+
+		case LINEAR:
+			for(int i = 0; i < (int)(len); i++){
+				vals[i] = (uint16_t)LINEAR_DECAY(i, coef, base) * sin(freqs[i] * i); // set all to base if no freq decay
+			}
+
+		case QUADRATIC:
+			for(int i = 0; i < (int)(len); i++){
+				vals[i] = (uint16_t)QUADRATIC_DECAY(i, coef, base) * sin(freqs[i] * i);  // set all to base if no freq decay
+			}
+
+		case EXPONENTIAL:
+			for(int i = 0; i < (int)(len); i++){
+				vals[i] =(uint16_t)EXP_DECAY(i, coef, base) * sin(freqs[i] * i);  // set all to base if no freq decay
+			}
+
+	}
+
+	return vals;
+
+}
+
 
 
 // declaring instrument
 // returns an instrument struct which the user can add to the track at their choosing
 
-struct transient_sample * init_transient_sample(unsigned int base_freq, unsigned int amp, int amp_decay_func,int amp_decay_coef, int freq_decay_func, int freq_decay_coef){
+struct transient_sample * init_transient_sample(unsigned int base_freq, unsigned int amp, int amp_decay_func,float amp_decay_coef, int freq_decay_func, float freq_decay_coef){
 	
 	struct transient_sample * ts = malloc(sizeof(struct transient_sample));
 
 	ts->base_freq = base_freq;
 	ts->amp = amp;
 	ts->amp_decay_func = amp_decay_func;
-	ts->amp_decay_coef = amp_decay_coef;
+	ts->amp_decay_coef = amp_decay_coef / 44100; //so the user can think in seconds
 	ts->freq_decay_func = freq_decay_func;
-	ts->freq_decay_coef = amp_decay_coef;
+	ts->freq_decay_coef = amp_decay_coef / 44100;
 
 	return ts;
 
 }
 
-uint8_t * render_transient(struct transient_sample * ts, unsigned int * sample_len){
+uint16_t * render_transient(struct transient_sample * ts, unsigned int * sample_len){
 
-	*sample_len = calculate_len(ts->amp, ts->amp_decay_func, ts->amp_decay_coef) * 2;
+	*sample_len = calculate_len(ts->amp, ts->amp_decay_func, ts->amp_decay_coef);
 
-	uint8_t * outbuf = malloc(*sample_len * 2); // 2 bytes per sample
-	uint8_t * freqs = malloc(*sample_len * 2); // one pass for frequencies, one for output
+	uint16_t * outbuf = malloc(*sample_len * 2); // 2 bytes per sample
+	uint16_t * freqs = malloc(*sample_len * 2); // one pass for frequencies, one for output
 
-	// implement calculation loops
+	freqs = calculate_freqs(ts->base_freq, ts->freq_decay_func, ts->freq_decay_coef, *sample_len);
 
-	return outbuf;
+	return calculate_vals(ts->amp, ts->amp_decay_func, ts->amp_decay_coef, *sample_len, freqs);
 
 }
 
--- src/instrument.h
+++ src/instrument.h
@@ -1,10 +1,11 @@
 #include <stdint.h>
+#include <math.h>
 #define E 2.71828
 
 // dacay func macros
-#define EXP_DECAY(x, c, a) (a*E**(-x*c))
-#define LINEAR_DECAY(x, c, a) (a*(1-(x*c)))
-#define QUADRATIC_DECAY(x, c, a) (a*(1-((c*x)**2)))
+#define EXP_DECAY(x, c, a) (a * pow(E,(-x * c)))
+#define LINEAR_DECAY(x, c, a) (a * (1-(x * c)))
+#define QUADRATIC_DECAY(x, c, a) (a * (1-(pow((c * x), 2))))
 
 // easier to work with
 enum decay_functions{
@@ -28,17 +29,17 @@ struct transient_sample{
 	unsigned int amp;
 	int amp_decay_func;
 	int amp_decay_coef;
-	int freq_decay_func;
-	int freq_decay_coef;
+	float freq_decay_func;
+	float freq_decay_coef;
 };
 
 
 
 // USER CALLED FUNCTIONS
 
-struct transient_sample * init_transient_sample(unsigned int base_freq, unsigned int amp, int amp_decay_func,int amp_decay_coef, int freq_decay_func, int freq_decay_coef);
+struct transient_sample * init_transient_sample(unsigned int base_freq, unsigned int amp, int amp_decay_func,float amp_decay_coef, int freq_decay_func, float freq_decay_coef);
 
 // INTERNAL FUNCTIONS
 
 // turns transient_sample struct into actual data to add to full data timeline
-uint8_t * render_transient(struct transient_sample * ts, unsigned int * sample_len); 
+uint16_t * render_transient(struct transient_sample * ts, unsigned int * sample_len); 
